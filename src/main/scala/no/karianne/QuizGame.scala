25fa62d70c76926f39f03b974b1ac75348817847
Karianne Berg <karianne.berg@objectware.no>
2010-09-05 10:47:58 +0200

    enda bedre begynnelse

--- src/main/scala/no/karianne/QuizGame.scala
+++ src/main/scala/no/karianne/QuizGame.scala
@@ -2,30 +2,14 @@ package no.karianne
 
 class QuizGame(val numOfQuestions : Int){
 
-  private val allQuestions = QuestionFactory.questions
-  private var currentQuestionNumber = 0
-  private var currentScore = 0
+  def isNotFinished = false
 
+  def nextQuestion = ""
 
-  def isNotFinished = !isFinished
+  def isValid(answer : String) = true
 
-  def isFinished = currentQuestionNumber == numOfQuestions
+  def answer(answer : String) = {}
 
-  def currentQuestion: Question = {
-    allQuestions(currentQuestionNumber - 1)
-  }
-
-  def nextQuestion = {
-    currentQuestionNumber += 1
-    currentQuestion
-  }
-
-  def isValid(answer : String) = currentQuestion.isValid(answer)
-
-  def answer(answer : String) = {
-    if(currentQuestion.isCorrect(answer)) currentScore += 1
-  }
-
-  def finalScore = currentScore
+  def finalScore = 0
 
 }
