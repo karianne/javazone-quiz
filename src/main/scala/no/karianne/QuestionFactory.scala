c11a8ca603a2252ce939eb5e014f9af8a13e7b9b
Karianne Berg <karianne.berg@objectware.no>
2011-02-05 18:42:01 +0100

    la til de filene som manglet

---
+++ src/main/scala/no/karianne/QuestionFactory.scala
@@ -0,0 +1,54 @@
+package no.karianne
+
+object QuestionFactory {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def questions = {
+    List(
+      Question("Dersom du uventet får Unit som returtype, hva har du glemt da?",
+        ("3", "=-tegn før metodekroppen"),
+        Map("1" -> "Å definere returtypen", "2" -> "Å skrive punktum")),
+      Question("Dersom du får en shitload med skikkelig sære feil, hva bør du gjøre da?",
+        ("3" -> "Bruke punktum og parenteser i metodekall"),
+        Map("1" -> "Lære deg Ruby i stedet", "2" -> "Vente til dagen etter")),
+      Question("""Hva er i veien dersom du får feilmeldingen "value question is not defined"?""",
+        ("3" -> "Du har glemt å skrive val eller var foran variabeldeklarasjonen"),
+        Map("1" -> "Du har glemt å definere variabelen", "2" -> "Kompilatoren har PMS")),
+      Question("Dersom det føles fristende å returnere null fra en metode, hva bør returverdien til metoden være da?",
+        ("3" -> "Option"),
+        Map("1" -> "Any", "2" -> "Nothing")),
+      Question("Har ninjaer noensinne hatt på seg stillongs??",
+        ("3" -> "Definitivt ikke"),
+        Map("1" -> "Hele tiden", "2" -> "Bare på tidlig 1800-tall"))
+      )
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
